989381912e7b585c77dd9985de5878fa583da36f
Jonathan Rivers <jrrivers@gmail.com>
2015-10-22 22:11:02 +0000

    Added content before tweaking contact futher

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -16,8 +16,8 @@ class ContactsController < ApplicationController
     end
     
     private
-    def contact_params
-        params.require(:contact).permit(:name, :email, :comments)
-    end
+        def contact_params
+            params.require(:contact).permit(:name, :email, :comments)
+        end
     
 end
