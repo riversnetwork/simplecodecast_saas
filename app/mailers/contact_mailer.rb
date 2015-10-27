30509df0df585890b8ee9f626bdb1512874934fc
Jonathan Rivers <jrrivers@gmail.com>
2015-10-27 00:57:52 +0000

    Added mailer and view

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,13 @@
+class ContactMailer < ActionMailer::Based
+    
+    default to: 'jrrivers@gmail.com'
+    
+    def contact_email(name,email,body)
+        @name = name
+        @email = email
+        @body = body
+        
+        mail(from: email, subject: 'Contact Form Message')
+    end
+end
+
